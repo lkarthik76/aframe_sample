89d4a9c679672e21295097c02d9af909db92ef33
Karthikeyan <lkarthik76@gmail.com>
2021-08-18 15:17:39 +0530

    Update play-on-click.js

--- js/play-on-click.js
+++ js/play-on-click.js
@@ -10,10 +10,15 @@ AFRAME.registerComponent('play-on-click', {
     window.removeEventListener('click', this.onClick);
   },
   onClick: function (evt) {
-    var videoEl = this.el.getAttribute('material').src;
-    if (!videoEl) { return; }
-    this.el.object3D.visible = true;
-    videoEl.load();
-    videoEl.play();
+//     var videoEl = this.el.getAttribute('material').src;
+//     if (!videoEl) { return; }
+//     this.el.object3D.visible = true;
+//     videoEl.load();
+//     videoEl.play();
+       var myVideo = document.querySelector('video');
+        if (!myVideo) { return; }
+        console.log(myVideo);
+        this.el.object3D.visible = true;
+        myVideo.play();
   }
 });
