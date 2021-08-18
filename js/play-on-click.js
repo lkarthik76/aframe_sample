01083d94a64ca33f3b740af61158ca6e4e16a083
Karthikeyan <lkarthik76@gmail.com>
2021-08-18 15:31:07 +0530

    Update play-on-click.js

--- js/play-on-click.js
+++ js/play-on-click.js
@@ -10,8 +10,8 @@ AFRAME.registerComponent('play-on-click', {
     window.removeEventListener('click', this.onClick);
   },
   onClick: function (evt) {
-     var videoEl = this.el.getAttribute('material').src;
-   if (!videoEl) { return; }
+   //  var videoEl = this.el.getAttribute('material').src;
+  // if (!videoEl) { return; }
 //     this.el.object3D.visible = true;
 //     videoEl.load();
 //     videoEl.play();
